f447e178f8d2bf67aa48014ac6a0f54963923fc2
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-06 14:15:42 +0900

    [FEAT] index.html 라우팅

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"scuba/client"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,16 +10,11 @@ func main() {
 	
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/containers", func(c *fiber.Ctx) error {
 		containers := client.GetContainers()
-		for _, container := range containers {
-			fmt.Println("Container ID:", container["Id"])
-			fmt.Println("Container Name:", container["Names"])
-			fmt.Println("Container Image:", container["Image"])
-		}
 		return c.JSON(containers)
 	})
 
-	
+	app.Static("/", "./pages")
 	app.Listen(":8989")
 }
